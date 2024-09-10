f2d567fb8b34618f2b3f63c14a0802c11fbb25f6
Rafael de Luca <deluca1712@gmail.com>
2024-09-10 16:58:51 -0300

    Beer Reader

---
+++ src/main/java/com/rafaeldeluca/backend_beer_consumers_suppliers/readers/BeerReader.java
@@ -0,0 +1,43 @@
+package com.rafaeldeluca.backend_beer_consumers_suppliers.readers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rafaeldeluca.backend_beer_consumers_suppliers.entities.Beer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class BeerReader {
+
+    final InputStream inputStream;
+
+    public BeerReader(String dataJsonFile) {
+        this.inputStream = this.getClass().getClassLoader().getResourceAsStream(dataJsonFile);
+    }
+
+    public List<Beer> streamToList() {
+
+        if (inputStream == null) {
+            return List.of();
+        }
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.disable(
+                DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+                DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES,
+                DeserializationFeature.FAIL_ON_INVALID_SUBTYPE
+        );
+
+        try {
+            return objectMapper.readValue(inputStream, new TypeReference<List<Beer>>() {
+            });
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            throw new RuntimeException("Can not read json File", ioException);
+        }
+  
+    }
+
+}
