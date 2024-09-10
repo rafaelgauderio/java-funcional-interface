7d2367bcc2a82e7930fbc64d93555166594dac43
Rafael de Luca <deluca1712@gmail.com>
2024-09-10 16:34:12 -0300

    create project

---
+++ src/main/java/com/rafaeldeluca/backend_beer_consumers_suppliers/BackendBeerConsumersSuppliersApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.backend_beer_consumers_suppliers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBeerConsumersSuppliersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBeerConsumersSuppliersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaeldeluca/backend_beer_consumers_suppliers/BackendBeerConsumersSuppliersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.backend_beer_consumers_suppliers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBeerConsumersSuppliersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
