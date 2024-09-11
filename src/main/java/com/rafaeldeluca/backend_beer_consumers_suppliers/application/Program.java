1d7ab360b52b23de689de59418bd2d164666d7d3
Rafael de Luca <deluca1712@gmail.com>
2024-09-11 11:40:27 -0300

    consumers

--- src/main/java/com/rafaeldeluca/backend_beer_consumers_suppliers/application/Program.java
+++ src/main/java/com/rafaeldeluca/backend_beer_consumers_suppliers/application/Program.java
@@ -14,13 +14,23 @@ public class Program {
     private void testConsumers(List<Beer> beerList) {
         // receive a data input and to do return anything
         // has to default methods (accept, andThen)
-        final Consumer<Beer> beerConsumer = beer -> System.out.println(beer);
+
+        System.out.println("\nPrinting without inform object type");
+        beerList.forEach(beer -> System.out.println(beer));
+
         System.out.println("\nUsing consumer");
+        final Consumer<Beer> beerConsumer = beer -> System.out.println(beer);
         beerList.forEach(beerConsumer);
+
+        System.out.println("\nSugar sintaxe");
+        beerList.forEach(System.out::println);
+
         System.out.println("\nforEach");
         for(Beer nickname : beerList) {
             System.out.println(nickname);
         }
+
+
     }
 
     public static void main(String[] args) {
