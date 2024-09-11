95dd855170660b10ab54df3132e39d4049b9a0d1
Rafael de Luca <deluca1712@gmail.com>
2024-09-11 17:52:07 -0300

    Testing Beer Supplier

--- src/main/java/com/rafaeldeluca/backend_beer_consumers_suppliers/application/Program.java
+++ src/main/java/com/rafaeldeluca/backend_beer_consumers_suppliers/application/Program.java
@@ -6,6 +6,7 @@ import com.rafaeldeluca.backend_beer_consumers_suppliers.services.BeerService;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Program {
 
@@ -34,6 +35,7 @@ public class Program {
 
     private void testBeerService(List<Beer> beerList) {
 
+        System.out.println("\nBeer Service");
         final Beer beer  = BeerService.getBeerFromFile("Any beer");
         if(beer !=null) {
             System.out.println(beer);
@@ -42,10 +44,25 @@ public class Program {
         }
     }
 
+    private void testSupliers (List<Beer> beerList) {
+        // supplier supplies value to a function
+        // supllier make possivbel to produce data and return it
+        // using Optinal it is not necessary to treat empty and blank responses
+        // suplier is Lazzy
+        final var beer = BeerService.getBeerFromFileUsingOptional("");
+        System.out.println("\nUsing Supplier");
+        final Supplier<Beer> beerSupplier = () -> beerList.get(1);
+        //System.out.println(beer.orElse(beerList.get(0)));
+        System.out.println(beer.orElseGet(beerSupplier));
+
+
+    }
+
     public static void main(String[] args) {
         final List<Beer> beerList = new BeerReader("beers.json").streamToList();
         Program program = new Program();
         //program.testConsumers(beerList);
-        program.testBeerService(beerList);
+        //program.testBeerService(beerList);
+        program.testSupliers(beerList);
     }
 }
